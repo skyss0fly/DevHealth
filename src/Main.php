e68abb30c8c5a832f72aaa515718cd2e8e77c376
kostamax27 <69608272+kostamax27@users.noreply.github.com>
2022-07-29 00:10:56 +0300

    Update Main.php

--- src/Main.php
+++ src/Main.php
@@ -7,22 +7,24 @@ namespace skyss0fly\DevHealthPM4;
 use pocketmine\plugin\PluginBase;
 
 class Main extends PluginBase{
-	public function onLoad(): void
-    {
-        if ($this->debugMode === true) {
-            $this->getLogger()->info(TextFormat::DARK_BLUE . "DevHealthPM4 Has Successfully loaded");
-        }
-    }
- public function onEnable(): void
-    {
-
-        if ($this->debugMode === true) {
-            $this->getLogger()->info(TextFormat::DARK_GREEN . "Make sure to Take breaks when developing!");
-        }
-function onDisable(): void
-     }
-        if ($this->debugMode === true) {
-            $this->getLogger()->info(TextFormat::DARK_RED . "Goodbye!!");
-        }
-    }
+	
+	private bool $debugMode = true;
+	
+	public function onLoad(): void {
+		if ($this->debugMode) {
+			$this->getLogger()->info(TextFormat::DARK_BLUE . "DevHealthPM4 Has Successfully loaded");
+		}
+	}
+	
+	public function onEnable(): void {
+		if ($this->debugMode) {
+			$this->getLogger()->info(TextFormat::DARK_GREEN . "Make sure to Take breaks when developing!");
+		}
+	}
+	
+	public function onDisable(): void {
+		if ($this->debugMode) {
+			$this->getLogger()->info(TextFormat::DARK_RED . "Goodbye!!");
+		}
+	}
 }
